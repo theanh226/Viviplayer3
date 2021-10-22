1991c3a600899f950f41a435394343452ac3818d
The Anh Tran <theanh0226@gmail.com>
2021-10-22 17:41:37 +0200

    final without testing

---
+++ src/App.js
@@ -0,0 +1,150 @@
+import React, { useRef, useState, useEffect } from "react";
+import { Button, Rate } from "antd";
+import Segment from "./Segment";
+const markersDefault = [
+  {
+    time: 2,
+    text: "Chapter 1",
+  },
+  {
+    time: 16,
+    text: "Chapter 2",
+  },
+  {
+    time: 23.6,
+    text: "Chapter 3",
+  },
+  {
+    time: 50,
+    text: "Chapter 4",
+  },
+  {
+    time: 136,
+    text: "Chapter 5",
+  },
+  {
+    time: 226,
+    text: "Chapter 6",
+  },
+];
+const App = () => {
+  const [marker, setMarkers] = useState();
+  const [player, setPlayer] = useState(null);
+  const [manuellSegment, setManuellSegment] = useState();
+  const [updateComponent, setUpdateComponent] = useState(false);
+  let getSegmentFromStogare = localStorage.getItem("segmentSetting");
+  console.log("typeof Segment", typeof JSON.parse(getSegmentFromStogare));
+  console.log("typeof markersDefault", typeof markersDefault);
+  console.log(updateComponent);
+  const videoRef = useRef(null);
+  // set state for default markers and video
+  useEffect(() => {
+    setMarkers(markersDefault);
+    if (videoRef.current != null) {
+      setPlayer(videoRef.current);
+    }
+  }, []);
+
+  // useEffect(() => {
+  //   buildMarkers(player, marker);
+  // }, [player, marker]);
+
+  useEffect(() => {
+    setManuellSegment(JSON.parse(getSegmentFromStogare));
+    buildMarkers(player, manuellSegment);
+    console.log(manuellSegment);
+  }, [getSegmentFromStogare]);
+  // let buildMarkers;
+  const jumpToChapter = (video, { text, time }) => {
+    video.currentTime = time;
+    return video;
+  };
+
+  // const notiChapter = (text, time) => {
+  //   return (
+  //     <Modal title="Basic Modal" visible={isModalVisible}>
+  //       <h3>
+  //         {text} - {time} second
+  //       </h3>
+  //     </Modal>
+  //   );
+  // };
+
+  // if (marker !== null && marker !== undefined) {
+  //   console.log("marker inside", marker);
+  //   console.log("typeof marker inside", typeof marker);
+  //   buildMarkers = marker.map((chapter, index) => {
+  //     return (
+  //       <div key={index}>
+  //         <Button
+  //           type="primary"
+  //           ghost
+  //           onClick={() => jumpToChapter(player, chapter)}
+  //         >
+  //           {chapter.text} - {chapter.time} seconds
+  //         </Button>
+  //       </div>
+  //     );
+  //   });
+  // }
+
+  const buildMarkers = (video, segmentObj) => {
+    // console.log("build markers", segmentObj, video);
+    if (segmentObj === undefined || segmentObj === null) return;
+    let markersBuiled = segmentObj.map((chapter, index) => {
+      return (
+        <Button
+          type="primary"
+          ghost
+          onClick={() => jumpToChapter(video, chapter)}
+        >
+          {chapter.text} - {chapter.time} seconds
+        </Button>
+      );
+    });
+    return markersBuiled;
+  };
+  return (
+    <div className="App">
+      <video
+        id="test_video"
+        ref={videoRef}
+        controls
+        preload="none"
+        className="video-js vjs-default-skin"
+        width="640"
+        height="264"
+      >
+        {/* <source src="http://vjs.zencdn.net/v/oceans.mp4" type="video/mp4" />
+
+        <source src="http://vjs.zencdn.net/v/oceans.webm" type="video/webm" /> */}
+        <source
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <div>
+        Video rating: <Rate />
+      </div>
+      <ul id="marker-list"></ul>
+      <h2>Chapter</h2>
+      {/* {marker === undefined || marker === null
+        ? "Undivided video"
+        : buildMarkers} */}
+      {marker === null || marker === undefined
+        ? "undivided video"
+        : buildMarkers(
+            player,
+            manuellSegment !== null ? manuellSegment : marker
+          )}
+      <div>
+        <Segment
+          updateSegment={updateComponent}
+          setUpdateSegment={setUpdateComponent}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Segment.js
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import { Form, Input, InputNumber, Button, Space } from "antd";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+
+const Segment = ({ updateSegment, setUpdateSegment }) => {
+  console.log(updateSegment);
+  const onFinish = (values) => {
+    console.log("Received values of form:", values);
+    console.log(values.users);
+    localStorage.setItem("segmentSetting", JSON.stringify(values.users));
+    setUpdateSegment(!updateSegment);
+  };
+  // useEffect(() => {
+  //   return () => {
+  //     setUpdateComponent(!updateSegment);
+  //   };
+  // }, [setUpdateComponent]);
+  return (
+    <div>
+      <h2>Video Segmentieren</h2>
+      <Form
+        name="dynamic_form_nest_item"
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.List name="users">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, fieldKey, ...restField }) => (
+                <Space
+                  key={key}
+                  style={{ display: "flex", marginBottom: 8 }}
+                  align="baseline"
+                >
+                  <Form.Item
+                    {...restField}
+                    name={[name, "text"]}
+                    fieldKey={[fieldKey, "text"]}
+                    rules={[
+                      { required: true, message: "Missing name of chapter" },
+                    ]}
+                  >
+                    <Input placeholder="Name of chapter" />
+                  </Form.Item>
+                  <Form.Item
+                    {...restField}
+                    name={[name, "time"]}
+                    fieldKey={[fieldKey, "time"]}
+                    rules={[{ required: true, message: "Missing time" }]}
+                  >
+                    <InputNumber placeholder="Time (seconds)" />
+                  </Form.Item>
+                  <MinusCircleOutlined onClick={() => remove(name)} />
+                </Space>
+              ))}
+              <Form.Item>
+                <Button
+                  type="dashed"
+                  onClick={() => add()}
+                  block
+                  icon={<PlusOutlined />}
+                >
+                  Add Chapter
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Sengment
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Segment;
